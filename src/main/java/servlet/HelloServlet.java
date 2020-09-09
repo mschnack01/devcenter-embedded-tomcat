13023209b63f81e990f2b7e16d1b8ef283918d3a
MathiasSchnack <mathiasschnack@gmail.com>
2020-09-09 17:14:50 +0200

    asd

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -1,7 +1,14 @@
 package servlet;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+
 import java.io.IOException;
 
+
+/*
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
@@ -9,11 +16,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
 @WebServlet(
         name = "MyServlet", 
         urlPatterns = {"/hello"}
     )
-public class HelloServlet extends HttpServlet {
+
+*/
+
+
+@Path("/")
+
+public class HelloServlet /* extends HttpServlet*/ {
+
+    @GET
+    public Response defaultGetMethod(){
+        return Response.status(200).type("text/html").entity("Welcome to DIS exam 2019. Good luck!").build();
+    }
+
+    /*
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -23,5 +45,7 @@ public class HelloServlet extends HttpServlet {
         out.flush();
         out.close();
     }
+
+    */
     
 }
